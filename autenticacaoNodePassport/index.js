95f4daa9d7bb5104b50665ffb4fdbadd0e26d1b7
Paulo França <paulofranca.contato@gmail.com>
2018-08-17 23:43:31 -0300

    - conhecendo passport e criando arquitetura do projeto - primeira autenticacao com http basic

---
+++ autenticacaoNodePassport/index.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const methodOverride = require('method-override')
+const mongoose = require('mongoose')
+const passport = require('passport')
+
+const app = express()
+
+/** PASSPORT BASIC */
+//passport.use(require('./src/auth/basic'))
+//app.get('*', passport.authenticate('basic', { session: false }))
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(passport.initialize())
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, 'src/view'))
+
+require('./src/index')(app)
+
+mongoose.connect('mongodb://localhost:27017/authNodePassport')
+
+app.listen(3000, () => {
+  console.log('Express started')
+})
---
+++ autenticacaoNodePassport/src/controller/main/main.js
@@ -0,0 +1,5 @@
+module.exports = (req, res) => {
+
+  // return res.json({ msg: 'Hello from express.js + passport' })
+  return res.render('main/index')
+}
